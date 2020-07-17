2ce1234bce5c206938ebd448bf496fab750d3fd3
Machoka Daisy <machokadaisy25@gmail.com>
2020-07-17 17:39:48 +0300

    add array for male names

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,3 @@
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku','Yaw', 'Kofi', 'Kwame']
