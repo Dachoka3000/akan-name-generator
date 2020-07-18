7364fd5ce13b786ae9c77150282927837cf9037f
Machoka Daisy <machokadaisy25@gmail.com>
2020-07-18 03:18:28 +0300

    add variables for month and year of birth

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,25 @@
-function fn() {}
-    var d= new Date();
-
-    var gender= document.getElementsByName("gender");
-    var date= parseInt(document.getElementById("date").value);
-    var month= parseInt(document.getElementById("month").value);
-    var year= parseInt(document.getElementById("year").value);
-    var form1= document.getElementById("genderForm");
-
-
-
-
-    if(date<0 || date>31) {
-        document.getElementsByClassName("error").innerHtml = "Enter a valid date";
+var myFirstFunction= function(){
+    var birthday= new Date();
+    var dateOfBirth=parseInt(document.getElementById("date"));
+    var monthOfBirth=parseInt(document.getElementById("month"));
+    var yearOfBirth=parseInt(document.getElementById("year"));
+    
+    if (date <0 || date >31) {
+        document.getElementsByClassName("dateError").innerHTML="Please enter valid date";
         return false;
     } else {
-        d.setDate(date);
+        birthday.setDate(dateOfBirth)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku','Yaw', 'Kofi', 'Kwame'];
+    if (month<0 || month>12) {
+        document.getElementsByClassName("monthError").innerHTML="Please enter valid month";
+        return false;
+    } else {
+        birthday.setMonth(monthOfBirth);
+    }
+    if (year<0) {
+        document.getElementsByClassName("yearError").innerHTML="Please enter valid year";
+        return false;
+    } else {
+        birthday.setFullYear(yearOfBirth);
+    }
+}
