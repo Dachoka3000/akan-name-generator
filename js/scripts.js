2ab69c217a29ca344007d375d167916ebdb33e8c
Machoka Daisy <machokadaisy25@gmail.com>
2020-07-18 23:25:18 +0300

    add a formula to calculate day  of the week

--- js/scripts.js
+++ js/scripts.js
@@ -17,36 +17,43 @@ var myBirthdayFunction= function(){
     var yearOfBirth=document.getElementById("year");
     
     if (dateOfBirth.value<0 || dateOfBirth.value >31) {
-        document.getElementById("dateError").innerHTML="Please enter valid date";
+        document.getElementById("dateError").innerHTML="*Please enter valid date";
         return false;
     } else {
         "";
     }
     if (monthOfBirth.value<0 || monthOfBirth.value>12) {
-       document.getElementById("monthError").innerHTML="Please enter valid month";
+       document.getElementById("monthError").innerHTML="*Please enter valid month";
         return false;
     } else {
         "";
     }
     if (yearOfBirth.value<0 || yearOfBirth.value>2020 ) {
-        document.getElementById("yearError").innerHTML="Please enter valid year";
+        document.getElementById("yearError").innerHTML="*Please enter valid year";
+        return false;
     } else {
         "";
-    }
+    };
 };
-    var day=birthday.getDay();
+
+var getAkanName= function(){
+    
     var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     var femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-
-    var mySecondFunction= function() {
-        var male=document.getElementById("male");
-        var female=document.getElementById("female");
-        if (male.checked===true){
-            return male.value;
-        }else if (female.checked===true) {
-            return female.value;
-        }else {
-            alert("Please select gender");
-        }
-    }
+    var maleUser=document.getElementById("male");
+    var femaleUser=document.getElementById("female");
+    
+    var c= (document.getElementById("year").value)/100; //represents century;
+    var y= (document.getElementById("year").value)%100; // represents year born
+    var m=document.getElementById("month").value;//represents the month the user was born
+    var dt=document.getElementById("date").value;//represents the date of the month the user was born
+    var dayOfWeek= (((c/4)-2*c-1)+((5*y/4))+((26*(m+1)/10))+dt)%7;
+    
+    if (maleUser.checked===true && dayOfWeek<=0) {
+        return days[0];
+        alert("Your akan name is "+ maleNames[0]);
+    } else if (maleUser.checked===true && dayOfWeek<=1)
+    
+    
+}
