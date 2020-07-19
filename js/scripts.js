b37f706f2c9f2dbfe6213544af7cd4069e9629ba
Machoka Daisy <machokadaisy25@gmail.com>
2020-07-19 12:47:57 +0300

    add a new formula to calculate akan name

--- js/scripts.js
+++ js/scripts.js
@@ -46,38 +46,38 @@ var getAkanName= function(){
     var y= (parseInt(document.getElementById("yearOfBirth").value))%100; // represents year born
     var m=parseInt(document.getElementById("monthOfBirth").value);//represents the month the user was born
     var dt=parseInt(document.getElementById("dateOfBirth").value);//represents the date of the month the user was born
-    var dayOfWeek= 1
+    var dayOfWeek= (((c/4)-2*c-1)+((5*y/4))+((26*(m+1)/10))+dt)%7
 
     var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     var femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
     
     
-    if (maleUser.checked===true && dayOfWeek==0) {
+    if (maleUser.checked===true && dayOfWeek<=0) {
         alert("Your akan name is "+ maleNames[0]);
-    } else if (maleUser.checked===true && dayOfWeek==1) {
+    } else if (maleUser.checked===true && dayOfWeek<=1) {
         alert("Your akan name is "+ maleNames[1]);
-    } else if (maleUser.checked===true && dayOfWeek==2) {
+    } else if (maleUser.checked===true && dayOfWeek<=2) {
         alert("Your akan name is "+ maleNames[2]);
-    } else if (maleUser.checked===true && dayOfWeek==3) {
+    } else if (maleUser.checked===true && dayOfWeek<=3) {
         alert("Your akan name is "+ maleNames[3]);
-    } else if (maleUser.checked===true && dayOfWeek==4) {
+    } else if (maleUser.checked===true && dayOfWeek<=4) {
         alert("Your akan name is "+ maleNames[4]);
-    } else if (maleUser.checked===true && dayOfWeek==5) {
+    } else if (maleUser.checked===true && dayOfWeek<=5) {
         alert("Your akan name is "+ maleNames[5]);
-    } else if (maleUser.checked===true && dayOfWeek==6) {
+    } else if (maleUser.checked===true && dayOfWeek<=6) {
         alert("Your akan name is "+maleNames[6]);
-    } else if (femaleUser.checked===true && dayOfWeek==0) {
+    } else if (femaleUser.checked===true && dayOfWeek<=0) {
         alert("Your akan name is "+ femaleNames[0]);
-    } else if (femaleUser.checked===true && dayOfWeek==1) {
+    } else if (femaleUser.checked===true && dayOfWeek<=1) {
         alert("Your akan name is "+ femaleNames[1]);
-    } else if (femaleUser.checked===true && dayOfWeek==2) {
+    } else if (femaleUser.checked===true && dayOfWeek<=2) {
         alert("Your akan name is "+ femaleNames[2]);
-    } else if (femaleUser.checked===true && dayOfWeek==3) {
+    } else if (femaleUser.checked===true && dayOfWeek<=3) {
         alert("Your akan name is "+femaleNames[3]);
-    } else if (femaleUser.checked===true && dayOfWeek==4) {
+    } else if (femaleUser.checked===true && dayOfWeek<=4) {
         alert("Your akan name is "+femaleNames[4]);
-    } else if (femaleUser.checked===true && dayOfWeek==5) {
+    } else if (femaleUser.checked===true && dayOfWeek<=5) {
         alert("Your akan name is "+femaleNames[5]);
     } else if (femaleUser.checked===true && dayOfWeek<=6) {
         alert("Your akan name is "+ femaleNames[6]);
